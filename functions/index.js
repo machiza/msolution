b04035fa8a3c9b5b8678dcf7e6eb9d3e866a842e
machiza <machizaarmando@gmail.com>
2018-07-20 12:08:54 +0200

    Configuração do template engine

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,11 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
+app.engine('.hbs', exphbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('views', './views');
+app.set('view engine', 'hbs');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
